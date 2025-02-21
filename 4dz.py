44a55815330faf5fbb1c20d6daf2bf7347e49909
JavaP <javad.huseynov2307@gmail.com>
2025-02-21 15:02:36 +0400

    Update 4dz.py

--- 4dz.py
+++ 4dz.py
@@ -1,1 +1,20 @@
-
+result = []
+def divider(a, b):
+ if a < b:
+    raise ValueError("vALUEeRROR")
+ if b > 100:
+    raise IndexError('iNDEXERROR')
+ return a / b
+data = {10: 2, 2: 5, "123": 4, 18: 0, 8 : 4}
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except ZeroDivisionError:
+        print("деление на ноль")
+    except ValueError:
+        print("ошибка ввода")
+    except NameError:
+        print("плохое имя")
+        
+        
